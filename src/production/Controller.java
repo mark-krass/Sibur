a08ae42c0dd6df4dd88269e3436a9685ed3c616e
degra.dance <goto97@mail.ru>
2018-12-01 23:08:17 +0300

    legacy code

--- src/production/Controller.java
+++ src/production/Controller.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 import javafx.event.ActionEvent;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
 public class Controller {
     @FXML
@@ -11,6 +14,9 @@ public class Controller {
 
     @FXML
     private void click(ActionEvent event){
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println("mat = " + mat.dump());
         btn.setText("AaAAaaaAAaaAAAaa");
     }
 }
